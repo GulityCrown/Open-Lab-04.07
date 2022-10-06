b005194c54d36e1847b0f1ec032798a0afdc6355
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-10-06 08:19:05 +0200

    Update StringTools.cs

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._07
 {
@@ -6,7 +6,15 @@ namespace Open_Lab_04._07
     {
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            char[] c = original.ToCharArray();
+            string reversed = "";
+
+            for (int i = original.Length - 1; i >= 0; i--)
+            {                         
+                    reversed += c[i];
+            }
+            
+            return reversed;
         }
     }
 }
